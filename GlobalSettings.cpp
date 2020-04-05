55a011f1dba28a9fa469d87b9a05530b582b06ea
Cavalex <42481044+Cavalex@users.noreply.github.com>
2020-04-05 17:56:05 +0100

    Cleaned up the code a bit

--- GlobalSettings.cpp
+++ GlobalSettings.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <conio.h>
 
+// Variável para evitar incluir o ficheiro várias vezes
 #ifndef GLOBAL_CPP
 #define GLOBAL_CPP
 
@@ -18,18 +19,44 @@
 // Estas estão:
 #define KEY_X 120
 
-
 #define HEIGHT 35
 #define WIDTH 75
 
-
 #define chPlayer 'P'
 #define bgChar ' '
+
+using namespace std;
 
 void SetCursorPosition(int x, int y){
 	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD pos = {x,y};
 	SetConsoleCursorPosition(output, pos);
+}
+
+void SetConsoleSize(int width, int height){
+	HANDLE wHnd;    // Handle to write to the console.
+	HANDLE rHnd;
+
+	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	rHnd = GetStdHandle(STD_INPUT_HANDLE);
+    // Change the window title:
+    SetConsoleTitle("Space Invaders!");
+    
+    // Set up the required window size:
+    SMALL_RECT windowSize = {0, 0, width - 1, height - 1};
+    COORD bufferSize = {width, height};
+    
+    SetConsoleWindowInfo(wHnd, 1, &windowSize);
+    SetConsoleScreenBufferSize(wHnd, bufferSize);
+}
+
+void ClearScreen(const string& ch){
+	for (int y = 0; y < HEIGHT; y++){
+		for (int x = 0; x < WIDTH; x++){
+			SetCursorPosition(x, y);
+			cout << ch;
+		}
+	}
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,117 +6,36 @@
 #include <conio.h>
 
 #include "Entity.cpp"
+// O Menu ainda não está a dar e não nos precisamos de preocupar com isso agora
 //#include "Menu.cpp"
 #include "Player.cpp"
+// Não podemos incluir a classe Shot pq já está incluída no Player.cpp
 //#include "Shot.cpp"
 //#include "GlobalSettings.cpp"
 
 using namespace std;
 
-void SetConsoleSize(int width, int height){
-	HANDLE wHnd;    // Handle to write to the console.
-	HANDLE rHnd;
-
-	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
-	rHnd = GetStdHandle(STD_INPUT_HANDLE);
-    // Change the window title:
-    SetConsoleTitle("Space Invaders!");
-    
-    // Set up the required window size:
-    SMALL_RECT windowSize = {0, 0, width - 1, height - 1};
-    COORD bufferSize = {width, height};
-    
-    SetConsoleWindowInfo(wHnd, 1, &windowSize);
-    SetConsoleScreenBufferSize(wHnd, bufferSize);
-}
-
-void ClearScreen(const string& ch){
-	for (int y = 0; y < HEIGHT; y++){
-		for (int x = 0; x < WIDTH; x++){
-			SetCursorPosition(x, y);
-			cout << ch;
-		}
-	}
-}
-
 // AGORA O JOGO
-/*
-void movePlayer(int x, int y, int oldX = 0, int oldY = 0){
-	SetCursorPosition(oldX, oldY);
-	cout << bgChar;
-	SetCursorPosition(x, y);
-	cout << chPlayer;
-}
-*/
 int main(){
 	
 	// Mudar o tamanho da consola
 	SetConsoleSize(WIDTH, HEIGHT);
-	//////////////////////////////
-	/*
-	int initX = WIDTH / 2;
-	int initY = HEIGHT / 2;
-	int oldX = initX;
-	int oldY = initY;
-	int x = initX;
-	int y = initY;
-	*/
 	
-	// NÃO ESTÁ A DAR
-	// Talvez seja melhor não colocar o menu numa classe
-	/*
-	Menu menu = Menu();
-	menu.showOptions();
-	*/
-	
-	/*
-	// posição inicial do jogador
-	movePlayer(x, y);
-		
-	char key = getch();
-    int value = key;
-	*/
-	
-	// Desenhar esta entity
 	Player hero = Player(WIDTH / 2, HEIGHT - 2, 1, 0, 'O', "Cavalex", 0);
+	hero.drawEntity();
+	
 	Entity e = Entity(50, 25, 2, 2, 'X');
 	e.drawEntity();
 	e.moveTo(5, 5);
-	hero.shoot(1, 1, '|', 5);
+	// Ainda não consegue disparar, ver função Player::shoot()
+	// hero.shoot(1, 1, '|', 5);
 	
-	////////////////////// Game Loop
-    //while(value != KEY_X){
+	//Game Loop
     while(true){
     	hero.moveAction();
-    	
-    	/*
-    	// Ultimos valores do player, para serem "limpos"
-		oldX = x;
-        oldY = y;
-    	
-    	// movimento
-        switch(getch()){
-	        case KEY_UP:
-	            y -= 1;
-	            break;
-	        case KEY_DOWN:
-	            y += 1;
-	            break;
-	        case KEY_LEFT:
-	            x -= 1;
-				break;
-	        case KEY_RIGHT:
-	            x += 1;
-				break;
-        }
-		
-		movePlayer(x, y, oldX, oldY);
-		
-        key = getch();
-        value = key;
-        */
     }
 	
+	// Pausar consola até o user premir o Enter
 	cin.get();
 	return 0;
 }
